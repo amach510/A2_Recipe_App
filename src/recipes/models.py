a7f9bffb18e2b8fb1d430808118d92b5ba8f736e
Anthony <amach510@gmail.com>
2024-07-23 13:04:05 -0700

    corrected syntax

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -13,7 +13,7 @@ class Recipe(models.Model):
     cooking_time = models.IntegerField(help_text="Enter the cooking time in minutes", default=0)
     ingredients = models.CharField(max_length=250, help_text= "Enter each ingredient separated by a comma")
     difficulty = models.CharField(max_length=120, choices=difficulty_choices, default='Easy')
-    pic = models.ImageField(upload_to='recipes', help_text= "Upload Image (Min.250px)" default='no_picture.jpg')
+    pic = models.ImageField(upload_to='recipes', help_text= "Upload Image (Min.250px)", default='no_picture.jpg')
     
     def __str__(self):
         return f"{self.name} - {self.difficulty} - {self.cooking_time}"
