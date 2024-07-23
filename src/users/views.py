16983626da52e0141acf79299bd635e909db31be
Anthony <amach510@gmail.com>
2024-07-23 13:38:12 -0700

    added users urls.py and updated views.py

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+def profile(request):
+    user = User.objects.first()  # Retrieve the first user
+    context = {'user': user}
+    return render(request, 'users/users_profile.html', context)
